c84c980f7b551b8195b3515ccdfddd308adfa5fd
neverneeth <navaneethvsankar@gmail.com>
2025-02-03 20:41:37 +0530

    Grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
+const container = document.querySelector("#container"),  
+    tile = document.querySelector(".tile");
 
+for(let i = 0; i < 1599; i++) {
+  container.appendChild(tile.cloneNode());
+}
+
+document.querySelectorAll('.tile').forEach(tile => {
+    tile.addEventListener('click', function() {
+        this.classList.toggle('active');
+    });
+});
